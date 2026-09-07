ac7d640d3025e9b8271c4a8091ed705172dff44f
rvarshitha520@gmail.com <rvarshitha520@gmail.com>
2026-09-07 10:56:53 +0530

    first commit

--- TestFact.java
+++ TestFact.java
@@ -1,13 +1,13 @@
 
-public class TestFacorial {
+public class TestFact {
 
     public static void main(String[] args) {
         try {
-            if (Factorial.factorial(5) != 120) {
+            if (Fact.factorial(5) != 120) {
                 throw new AssertionError("Test failed for input 5");
             }
 
-            if (Factorial.factorial(0) != 1) {
+            if (Fact.factorial(0) != 1) {
                 throw new AssertionError("Test failed for input 0");
             }
 
